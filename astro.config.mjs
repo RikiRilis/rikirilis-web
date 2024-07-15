579db59887f0d6791051512b8a0e8a07cb8d5482
RikiRilis <thewhitzip@gmail.com>
2024-07-14 22:32:34 -0400

    Dotenv dependence added

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,21 +2,24 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  i18n: {
-    defaultLocale: "en",
-    locales: ["en", "es"],
-    routing: {
-      prefixDefaultLocale: false,
-    },
-  },
-  output: "server",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
+	integrations: [tailwind(), react()],
+	i18n: {
+		defaultLocale: "en",
+		locales: ["en", "es"],
+		routing: {
+			prefixDefaultLocale: false,
+		},
+	},
+	output: "server",
+	adapter: vercel({
+		webAnalytics: {
+			enabled: true,
+		},
+	}),
 });
