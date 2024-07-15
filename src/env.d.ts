72f385832b8a0634b1341361045fff0f457bbb61
RikiRilis <thewhitzip@gmail.com>
2024-07-14 22:34:47 -0400

    Supabase import meta env added for environment variables

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,6 +4,11 @@ interface Window {
 	getThemePreference(): "dark" | "light";
 }
 
+interface ImportMetaEnv {
+	readonly SECRET_SUPABASE_URL: string;
+	readonly SECRET_SUPABASE_ANON_KEY: string;
+}
+
 interface ImportMeta {
 	readonly env: ImportMetaEnv;
 }
