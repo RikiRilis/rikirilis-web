f292c7fd13dfe695fa4ef7a8cd198db33e8f3a4e
RikiRilis <thewhitzip@gmail.com>
2024-07-16 10:54:33 -0400

    Dotenv removed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,6 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
-import * as dotenv from "dotenv";
-
-dotenv.config();
 
 // https://astro.build/config
 export default defineConfig({
