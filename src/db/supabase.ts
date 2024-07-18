35fb18f5f249a7f139c5f3b275ac3e68cf3b755f
RikiRilis <thewhitzip@gmail.com>
2024-07-18 14:13:04 -0400

    Names of env variables changed

--- src/db/supabase.ts
+++ src/db/supabase.ts
@@ -1,6 +1,19 @@
+import type { Story } from "@/interfaces/story";
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-	import.meta.env.SECRET_SUPABASE_URL,
-	import.meta.env.SECRET_SUPABASE_ANON_KEY
+	import.meta.env.SUPABASE_URL,
+	import.meta.env.SUPABASE_ANON_KEY
 );
+
+export const insertRow = async ({ title, content, lang, tags }: Story) => {
+	const { error } = await supabase.from("web_stories").insert({
+		title: title,
+		content: content,
+		tags: tags,
+		lang: lang,
+	});
+
+	if (error) return false;
+	return true;
+};
