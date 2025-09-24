a6ccfa71b92a312230dde8935e6d1c5255c22e5d
RikiRilis <thewhitzip@gmail.com>
2025-09-24 19:32:54 -0400

    Problems fixed

--- src/services/images.ts
+++ src/services/images.ts
@@ -2,8 +2,6 @@ import type { Images } from '@/interfaces/images'
 import { getI18N } from '@/languages/index'
 import { getImagesWithSize } from '@/libs/getImageSizes'
 
-export const getImages = (currentLocale: string = 'en'): Images[] => {
-	const i18n = getI18N({ currentLocale })
-
+export const getImages = (): Images[] => {
 	return getImagesWithSize('portraits')
 }
